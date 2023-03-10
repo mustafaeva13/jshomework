0b20682153e64ccf5945099902cb1259184c0de8
Efsane <mstafaeva113@gmail.com>
2023-03-10 01:35:37 -0800

    1st commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+var person=new Object({
+     name:"Babek",
+     surname:"hesenov",
+     age:"21"
+   
+})
+   
+console.log(person)
+
+//console.log(person)
+var person2=new Object({
+    name: "Elmiraa",
+    surname:"Eliyeva",
+    age:"34",
+    })
+    
+    //console.log(person2)
+    
+    var person3=new Object({
+    
+    name:"xelil",
+    surname:"Quluzade",
+    age:"17",
+    })
+    
+    //console.log(person3)
+    var person4=new Object({
+    
+        name:"Gulu",
+        surname:"Mustafaeva",
+        age:"23",
+    
+    })
